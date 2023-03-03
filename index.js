b2d6ca3a7d754a3fb2d1f4d8aff3184edc31e276
amanvermai30 <amancreative9598@gmail.com>
2023-03-03 15:45:06 +0530

    day-2

---
+++ index.js
@@ -0,0 +1,157 @@
+//  get vendor profile- get request
+
+const sessionKey = localStorage.getItem("sessionkey");
+console.log(sessionKey);
+
+let getProductData = async () => {
+  try {
+    const res = await fetch(
+      `http://shopeasy-env.eba-xkxpqfpn.ap-south-1.elasticbeanstalk.com/productController/viewallproducts`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    if (res.ok) {
+      console.log("success");
+      const data = await res.json();
+      return data;
+    } else {
+      const data = await res.json();
+      const error = JSON.stringify(data);
+      const msg = JSON.parse(error);
+      console.log(msg);
+      throw new Error(msg.message);
+    }
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+//   after calling I am appending data in html
+let appendData = (data) => {
+
+  // taking main container for append each product which is inside a tag
+  const mainContainer = document.querySelector(".product--items__container");
+
+  data.forEach((el, index) => {
+
+    // // creating a tag, img tag, p tag and span tag
+    const atag = document.createElement("a");
+    const img = document.createElement("img");
+    const p1 = document.createElement("p");
+    const p2 = document.createElement("p");
+    const span1 = document.createElement("span");
+    const span2 = document.createElement("span");
+
+    // now I will set class for atag p2 and span tag only
+    atag.classList.add("product--items__info")
+    p2.classList.add("price2")
+    span1.classList.add("price1");
+    span2.classList.add("offer");
+
+
+    // add data to all tags 
+    img.src = el.picture;
+    p1.innerText = el.productName;
+    p2.innerText = "Price: " + "₹" + el.afterDiscountPrice;
+    span1.textContent = el.marketPrice;
+    span2.textContent = el.discount + "%";
+
+    // by clicking any product it will redirect to singal product page 
+    atag.addEventListener("click", () => {
+
+      if (checkLogin() == false) {
+
+        const productId = el.productId;
+        localStorage.setItem('productId', productId);
+        window.location.href = "./customer assets/singal product/singalProduct.html";
+      }
+
+    });
+    // now I will append all tags 
+    atag.append(img, p1, p2, span1, span2);
+    mainContainer.append(atag);
+
+  })
+};
+
+// calling  api for vendor data after login
+window.addEventListener("load", async () => {
+
+  const customerData = await getProductData();
+  appendData(customerData);
+
+});
+
+// serch product by category
+const aTags = document.querySelectorAll('.productCatogary a');
+
+for (let i = 0; i < aTags.length; i++) {
+
+  aTags[i].addEventListener('click', function (event) {
+    event.preventDefault();
+    const clickedTag = event.target.textContent;
+    localStorage.setItem('category', clickedTag);
+
+    if (clickedTag == "Mens") {
+
+      if (checkLogin() == false) {
+        window.location.href = "./customer assets/category/mans/mans.html";
+      }
+
+
+    } else if (clickedTag == "Womans") {
+
+      if (checkLogin() == false) {
+        window.location.href = "./customer assets/category/womans/womans.html";
+      }
+
+
+    } else if (clickedTag == "Kids") {
+
+      if (checkLogin() == false) {
+        window.location.href = "/customer assets/category/kids/kids.html";
+      }
+
+
+    } else {
+
+      if (checkLogin() == false) {
+        window.location.href = "/customer assets/category/grocery/grocery.html";
+      }
+
+    }
+
+  });
+}
+
+
+function checkLogin() {
+
+  if (sessionKey == null) {
+    alert("You have not logged in please login first");
+    window.location.href = "./customer assets/customer login/login.html"
+    return true;
+
+  } else {
+    
+    return false;
+  }
+
+}
+
+// checking on main page
+if(sessionKey != null ){
+  const logoutbtn = document.querySelector(".logout");
+  logoutbtn.innerText = "logout"
+}
+
+
+
+
+
---
+++ vendor assets/dashboard/dashboard.js
@@ -0,0 +1,83 @@
+//  get vendor profile- get request
+
+const sessionKey = localStorage.getItem("sessionKey");
+console.log(sessionKey);
+
+let vendorLoggedin = async () => {
+    try {
+        const res = await fetch(
+            `http://shopeasy-env.eba-xkxpqfpn.ap-south-1.elasticbeanstalk.com//vendorController/singalvendor/${sessionKey}`,
+            {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        if (res.ok) {
+            console.log("success");
+            const data = await res.json();
+            return data;
+        } else {
+            const data = await res.json();
+            const error = JSON.stringify(data);
+            const msg = JSON.parse(error);
+            console.log(msg);
+            throw new Error(msg.message);
+        }
+    } catch (error) {
+        console.log(error);
+        throw new Error(error);
+    }
+};
+
+//   after calling I am appending data in html
+let appendData = (data) => {
+    // selecting from html and after that appending into it
+    const Imgcontainer = document.querySelector(".profile__img--container");
+    const profileContainer = document.querySelector(".profile__info");
+
+    // creating img tag
+    const img = document.createElement("img");
+    img.src = data.personalInfo.imageUrl;
+
+    // creating form tag, input tag and label tag
+    const form = document.createElement("form");
+    const btnDiv = document.createElement("div");
+    const btn = document.createElement("button");
+    const btn2 = document.createElement("button");
+    btn2.innerText = "Edit";
+    btn2.className = "btn";
+    btn.innerText = "Update";
+    btn.className = "btn";
+
+
+    const inputs = [];
+    const values = [data.venderName, data.personalInfo.phone, data.personalInfo.email, data.personalInfo.password, data.personalInfo.country, data.personalInfo.state, data.personalInfo.city, data.personalInfo.pincode,];
+
+    for (let i = 0; i < 8; i++) {
+
+        const input = document.createElement("input");
+        input.className = "form__info--data";
+
+        input.value = values[i];
+        inputs.push(input);
+        form.appendChild(input);
+    }
+
+    // appending input to form to profileContainer& img to imgcontainer
+    Imgcontainer.append(img);
+    btnDiv.append(btn,btn2);
+    form.appendChild(btnDiv)
+    profileContainer.append(form);
+    console.log(data)
+};
+
+// calling  api for vendor data after login
+window.addEventListener("load", async () => {
+    const vendorData = await vendorLoggedin();
+    appendData(vendorData);
+});
+
+
